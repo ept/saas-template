64067bafd16df459e37550db9743b29bc7a92fbd
Martin Kleppmann <martin@eptcomputing.com>
2009-08-14 20:47:27 +0100

    Don't log passwords; do log users.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,13 +10,22 @@ class ApplicationController < ActionController::Base
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
   # Scrub sensitive parameters from your log
-  # filter_parameter_logging :password
+  filter_parameter_logging :password, :password_confirmation
 
   before_filter :set_timezone
+  before_filter :log_user
 
   protected
 
   def set_timezone
     Time.zone = current_user.time_zone if current_user
+  end
+
+  def log_user
+    if logged_in?
+      logger.info "Logged in as user ##{current_user.id} (#{current_user.email})"
+    else
+      logger.info "Not logged in"
+    end
   end
 end
