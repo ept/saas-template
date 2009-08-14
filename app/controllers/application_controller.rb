d47561b33bedfc24d194b394418f8eccfd8f9b03
Martin Kleppmann <martin@eptcomputing.com>
2009-08-14 21:21:47 +0100

    Nicer handling of InvalidAuthenticityToken errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,4 +28,28 @@ class ApplicationController < ActionController::Base
       logger.info "Not logged in"
     end
   end
+
+  # Override ActionController's default exception handling to deal with +InvalidAuthenticityToken+ errors
+  # nicely. Most often, +InvalidAuthenticityToken+ happen as follows: the user has two browser tabs open,
+  # one with a form page loaded. In the other tab, they log out and log back in again (thus destroying
+  # their session and creating a new one). Back in the first tab, they try to submit the form, but that
+  # page's authenticity token is no longer valid. Instead of giving them a horrible error message,
+  # we should log them out and ask them to log in again.
+  def rescue_action(exception)
+    return super unless exception.kind_of? ActionController::InvalidAuthenticityToken
+    logger.info "Handling #{exception} by forcing fresh login"
+
+    # Forget about any renders or redirects previously called
+    erase_results if performed?
+
+    # Nuke their session
+    logout_killing_session!
+    kill_remember_cookie!
+
+    # Try to take them back where they came from when they log in again
+    flash[:error] = "Please log in."
+    redirect = {:return_to => (request.env["HTTP_REFERER"] || '').gsub(/.*:\/\/[^\/]*/, '')}
+		redirect[:protocol] = 'https' if Rails::configuration.https_login
+    redirect_to(login_url(redirect))
+  end
 end
