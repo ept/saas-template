3e88cf1736c019e7c31201ab51eed9bade12d7ba
Martin Kleppmann <martin@eptcomputing.com>
2009-10-17 16:15:36 +0100

    Some useful general-purpose helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -35,4 +35,59 @@ module ApplicationHelper
   def customer_path(customer)
     url_for :controller => :customers, :subdomain => customer.subdomain
   end
+
+
+  # Label for a checkbox or radio button, because ActionView's label helper is crap
+  # cf. ActionView::Helpers::FormTagHelper#radio_button_tag
+  def value_label(name, value, text)
+    pretty_tag_value = value.to_s.gsub(/\s/, "_").gsub(/(?!-)\W/, "").downcase
+    pretty_name = name.to_s.gsub(/\[/, "_").gsub(/\]/, "")
+    "<label for=\"#{pretty_name}_#{pretty_tag_value}\">#{h(text)}</label>"
+  end
+
+  # Generate Javascript to hide and show parts of a page based on the value of checkboxes
+  # or radio buttons.
+  def reveal( what, options )
+    prefix = options[:prefix]
+
+    @hide_and_show_data << if options[:is_one_of]
+      # radio button
+      name = "#{prefix}[#{options[:only_if]}]"
+
+      "  var radioGroup = $(':radio').filter(function(){ return $(this).attr('name') == '#{name}'; });\n" +
+      "  radioGroup.change(function(){\n" +
+      "    var checked = radioGroup.filter(':checked');\n" +
+      "    var value = (checked.length == 0) ? '' : checked.attr('value');\n" +
+      "    switch(value) {\n" +
+      options[:is_one_of].map{|val| "      case '#{val}':\n"}.join +
+      "        $('##{what}').show();\n" +
+      "        break;\n" +
+      "      default:\n" +
+      "        $('##{what}').hide();\n" +
+      "    }\n" +
+      "  }).change();\n"
+
+    else
+      # check box
+      negation = (options[:is].to_sym == :checked || options[:is] == true) ? '' : '!'
+
+      "  $('##{prefix}_#{options[:only_if]}').change(function(){\n" +
+      "    if(#{negation}$(this).attr('checked')) {\n" +
+      "      $('##{what}').slideDown(animSpeed);\n" +
+      "    } else {\n" +
+      "      $('##{what}').slideUp(animSpeed);\n" +
+      "    }\n" +
+      "  }).change();\n"
+    end
+  end
+
+  # Wrapper for reveal helper
+  def hide_and_show
+    @hide_and_show_data = ''
+    yield
+    "<script type=\"text/javascript\">\n" +
+    "$(function() {\n" +
+    @hide_and_show_data +
+    "});\n</script>\n"
+  end
 end
